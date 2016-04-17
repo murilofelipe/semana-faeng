ed086881061fe18be351c636de0defc3a193957d
Sandys Nunes <sandysnunes@gmail.com>
2016-04-17 05:21:28 -0400

    cadastro basico e qr code

---
+++ client/accounts.js
@@ -0,0 +1,8 @@
+
+
+// configure the special accounts user interface
+// by setting up some extra fields and specifying constraints
+// see:https://github.com/ianmartorell/meteor-accounts-ui-bootstrap-3/
+Accounts.ui.config({
+    requestPermissions: {}
+});
--- client/main.js
+++ client/main.js
@@ -3,20 +3,63 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+
+Template.insertInscricaoForm.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var nome  = event.target.nome.value;
+        console.log("atualizando" + nome);
+        var userId = Meteor.userId();
+        Inscricoes.insert({nome: nome, userId: userId});
+    }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+
+Template.updateInscricaoForm.events({
+    'submit form': function (event) {
+        event.preventDefault();
+        var nome  = event.target.nome.value;
+        var id = Inscricoes.findOne({userId:Meteor.userId()})._id;
+        Inscricoes.update({_id: id}, {$set: {nome:nome}});
+    }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.updateInscricaoForm.helpers({
+    nome: function () {
+        return Inscricoes.findOne({userId:Meteor.userId()}).nome;
+    }
+});
+
+
+Template.main.helpers({
+    temCadastro:function () {
+        return Inscricoes.findOne({userId:Meteor.userId()});
+    }
+});
+
+
+
+Template.qr.onRendered(function () {
+    $('#qrcode').qrcode({
+        size: 200,
+        text: "http://larsjung.de/qrcode"
+    });
 });
